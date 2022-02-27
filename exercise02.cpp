de204d66dbae9a43530da3b164231494d93010a7
Bhawantha Lakshan Athapattu <99388728+IT21253508@users.noreply.github.com>
2022-02-27 13:10:48 +0000

    IT21253508-Tute 03

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+//declare fuction
 int volume(int height, int width, int length);
 
-int main() {
+int main() 
+{
     int box1Height, box1Width, box1Length;
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
-    
+
+    //user inputs
     cout << "Enter Box 1 Height : ";
     cin >> box1Height;
+  
     cout << "Enter Box 1 Width : ";
     cin >> box1Width;
+  
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
+    cout << "Enter Box 2 Height : ";
     cin >> box2Height;
+    
     cout << "Enter Box 2 Width : ";
     cin >> box2Width;
+    
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
     
@@ -31,3 +39,7 @@ int main() {
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
